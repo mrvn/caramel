f310421ad9b1123d853364e53454b127406efa58
Goswin von Brederlow <goswin-v-b@web.de>
2015-01-04 00:39:09 +0100

    Lexer: comment and simplify LexerTypes

--- lexer/src/lexerTypes.ml
+++ lexer/src/lexerTypes.ml
@@ -17,35 +17,56 @@
 
 (* Token and attribute types as input for the types generator *)
 module type TA = sig
+  (* The lexer splits the input into tokens. Each token has an
+   * attribute giving extra data for the token. You usualy have a
+   * token Indent with attribute being the name of the
+   * identifier. Keywords usualy have a unit attribute.
+   *
+   * Example:
+   * type _ token = Ident : string token | Plus : unit token
+   * type _ attrib = Nil : unit attrib | String : string -> string * attrib
+   * 
+   * The tokens act as witness type for the attribute. The Ident token must
+   * have a string attrib, which will be checked by the type system at at
+   * compile time.
+   *)
   type 'a token
   type 'a attrib
+  (* for debug purposed *)
   val string_of_token : 'a token -> string
   val string_of_attrib : 'a attrib -> string
+  (* extract value from attribute, used later by the parser *)
   val value_of_attrib : 'a attrib -> 'a
 end
 
 (* Output of the types generator *)
 module type LexerTypes = sig
+  (* from TA *)
   type 'a token
   type 'a attrib
   val string_of_token : 'a token -> string
   val string_of_attrib : 'a attrib -> string
   val value_of_attrib : 'a attrib -> 'a
+
+  (* output type of the lexer (lexeme Input.t) *)
   type lexeme
+  (* helper function to create a lexeme *)
   val lexeme : 'a token -> 'a attrib -> lexeme
+  (* return Some attrib if the token matches the lexeme *)
   val attrib_opt : 'a token -> lexeme -> 'a attrib option
+  (* return true if the token matches the lexeme *)
   val has_token : 'a token -> lexeme -> bool
+  (* for debug purposes *)
   val string_of_lexeme : lexeme -> string
   val string_of_lexemes : lexeme Input.t -> string
 end
 
+(* create automatic types and helper functions *)
 module MAKE(TA : TA) : LexerTypes with type 'a token = 'a TA.token
 				  and type 'a attrib = 'a TA.attrib = struct
-  type 'a token = 'a TA.token
-  type 'a attrib = 'a TA.attrib
-  let string_of_token = TA.string_of_token
-  let string_of_attrib = TA.string_of_attrib
-  let value_of_attrib = TA.value_of_attrib
+  include TA
+
+  (* create universal type for lexemes *)
   module W = struct
     type 'a key = 'a token
     type 'a value = 'a attrib
@@ -54,27 +75,24 @@ module MAKE(TA : TA) : LexerTypes with type 'a token = 'a TA.token
   end
   module Boxed = Univ.Witnessed.MAKE(W)
   type lexeme = Boxed.t
+
+  (* helper function to create a lexeme *)
   let lexeme token attrib = Boxed.box token attrib
+
+  (* return Some attrib if the token matches the lexeme *)
   let attrib_opt token lexeme = Boxed.value_opt token lexeme
+
+  (* return true if the token matches the lexeme *)
   let has_token token lexeme =
     match Boxed.value_opt token lexeme with
     | None -> false
     | _ -> true
+
+  (* for debug purposes *)
   let string_of_lexeme lexeme =
     match Boxed.as_strings lexeme with
     | (k, "") -> k
     | (k, v) -> Printf.sprintf "(%s %s)" k v
-
-  let string_of_input printer input =
-    let rec loop sep str = function
-      | [] -> str
-      | (pos, symbol)::xs ->
-	loop
-	  " "
-	  (Printf.sprintf "%s%s [%s]" sep (printer symbol) (Pos.string_of_t pos))
-	  xs
-    in
-    loop "" "" input
 
   let string_of_lexemes lexemes =
     let (_, str) =
